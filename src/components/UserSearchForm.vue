9186c8085c21e9cad503dddfbea7d18c13d8340f
Andrey Aratov <madingame@yandex.ru>
2020-02-22 16:44:23 +0300

    added api request tests

--- src/components/UserSearchForm.vue
+++ src/components/UserSearchForm.vue
@@ -8,7 +8,7 @@
 <script>
 export default {
   name: 'UserSearchForm',
-  data() {
+  data () {
     return {
       username: ''
     }
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import UserView from '../views/UserView'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: []
+  routes: [
+    {
+      path: '/',
+      component: UserView
+    }
+  ]
 })
 
 export default router
